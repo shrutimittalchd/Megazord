33eb43efca9ab3a601f17ae87c9c552f9518b5a3
Unknown <mhird13@gmail.com>
2019-01-24 16:17:23 -0800

    Setting up basic structure

---
+++ megazord/src/main/java/ca/viu/csci331/megazord/AppMain.java
@@ -0,0 +1,13 @@
+package ca.viu.csci331.megazord;
+
+public class AppMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
